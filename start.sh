72b871900cd94f831481c7502e04c59b0b3fbd14
TACHEMO MAKUISSU <140599944+makuissu@users.noreply.github.com>
2026-04-15 02:35:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-mysql -h "$MYSQLHOST" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < "absencetrack(1).sql" 2>/dev/null || true
-php -S 0.0.0.0:8080 -t . router.php
+mysql -h "$MYSQLHOST" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" -e "CREATE DATABASE IF NOT EXISTS absencetrack;"
+mysql -h "$MYSQLHOST" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" absencetrack < "absencetrack(1).sql" 2>/dev/null || true
+php -S 0.0.0.0:8080 -t . router.ph
