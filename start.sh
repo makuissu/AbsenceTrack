5c2bceedd39d9ddd036d4bfe11d3d627afd5690e
TACHEMO MAKUISSU <140599944+makuissu@users.noreply.github.com>
2026-04-15 02:44:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ php -r "
 \$user = getenv('MYSQLUSER');
 \$pass = getenv('MYSQLPASSWORD');
 \$pdo = new PDO('mysql:host=' . \$host, \$user, \$pass);
-\$pdo->exec('CREATE DATABASE IF NOT EXISTS absencetrack');
+\$pdo->exec('DROP DATABASE IF EXISTS absencetrack');
+\$pdo->exec('CREATE DATABASE absencetrack');
 \$pdo = new PDO('mysql:host=' . \$host . ';dbname=absencetrack', \$user, \$pass);
 \$sql = file_get_contents('absencetrack(1).sql');
 \$pdo->exec(\$sql);
