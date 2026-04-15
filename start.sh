0b2e517c3f12858f110db245dbb26bdbdccb42f2
TACHEMO MAKUISSU <140599944+makuissu@users.noreply.github.com>
2026-04-15 02:31:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-php -S 0.0.0.0:8080 router.php
+mysql -h "$MYSQLHOST" -u "$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < absencetrack\(1\).sql 2>/dev/null || true
+php -S 0.0.0.0:8080 -t . router.php
